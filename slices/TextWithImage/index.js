5eeea3a869726931d676cfd0c4acde076c80e13c
Raphael Discky <zundria.putra@gmail.com>
2022-07-26 20:18:11 +0700

    feat: add left image with text

--- slices/TextWithImage/index.js
+++ slices/TextWithImage/index.js
@@ -1,28 +1,43 @@
-import * as prismicH from "@prismicio/helpers";
-import { PrismicRichText } from "@prismicio/react";
-import { PrismicNextImage } from "@prismicio/next";
+import * as prismicH from '@prismicio/helpers'
+import { PrismicRichText } from '@prismicio/react'
+import { PrismicNextImage } from '@prismicio/next'
 
-import { Bounded } from "../../components/Bounded";
+import { Bounded } from '../../components/Bounded'
 
 const TextWithImage = ({ slice }) => {
-  const image = slice.primary.image;
-
-  return (
-    <Bounded as="section" className="bg-white">
-      <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
+  const image = slice.primary.image
+  console.log(slice)
+  return slice.variation === 'leftImage' ? (
+    <Bounded as='section' className='bg-white'>
+      <div className='grid grid-cols-1 items-center gap-8 md:grid-cols-2'>
+        <div>
+          {prismicH.isFilled.image(image) && (
+            <div className='bg-gray-100'>
+              <PrismicNextImage field={image} layout='responsive' />
+            </div>
+          )}
+        </div>
+        <div>
+          <PrismicRichText field={slice.primary.text} />
+        </div>
+      </div>
+    </Bounded>
+  ) : (
+    <Bounded as='section' className='bg-white'>
+      <div className='grid grid-cols-1 items-center gap-8 md:grid-cols-2'>
         <div>
           <PrismicRichText field={slice.primary.text} />
         </div>
         <div>
           {prismicH.isFilled.image(image) && (
-            <div className="bg-gray-100">
-              <PrismicNextImage field={image} layout="responsive" />
+            <div className='bg-gray-100'>
+              <PrismicNextImage field={image} layout='responsive' />
             </div>
           )}
         </div>
       </div>
     </Bounded>
-  );
-};
+  )
+}
 
-export default TextWithImage;
+export default TextWithImage
